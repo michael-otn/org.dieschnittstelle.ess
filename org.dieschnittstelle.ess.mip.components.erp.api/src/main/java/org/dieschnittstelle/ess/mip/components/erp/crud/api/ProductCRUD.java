4743bec99186db562c821a467a692d97f1fc3858
zsvt9548 <zsvt9548@bht-berlin.de>
2026-06-01 19:22:42 +0200

    MIP + JPA kick-off

--- org.dieschnittstelle.ess.mip.components.erp.api/src/main/java/org/dieschnittstelle/ess/mip/components/erp/crud/api/ProductCRUD.java
+++ org.dieschnittstelle.ess.mip.components.erp.api/src/main/java/org/dieschnittstelle/ess/mip/components/erp/crud/api/ProductCRUD.java
@@ -2,6 +2,8 @@ package org.dieschnittstelle.ess.mip.components.erp.crud.api;
 
 import java.util.List;
 
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
 import org.dieschnittstelle.ess.entities.erp.AbstractProduct;
 import org.dieschnittstelle.ess.entities.erp.Campaign;
 
@@ -11,18 +13,32 @@ import org.dieschnittstelle.ess.entities.erp.Campaign;
  * See TouchpointCRUDImpl for an example bean with a similar scope of functionality
  */
 
+
+
+@Path("/products")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public interface ProductCRUD {
 
+	@POST
 	public AbstractProduct createProduct(AbstractProduct prod);
 
+	@GET
 	public List<AbstractProduct> readAllProducts();
 
+	@PUT
 	public AbstractProduct updateProduct(AbstractProduct update);
 
-	public AbstractProduct readProduct(long productID);
+	@GET
+	@Path("/{id}")
+	public AbstractProduct readProduct(@PathParam("id") long productID);
 
-	public boolean deleteProduct(long productID);
+	@DELETE
+	@Path("/{id}")
+	public boolean deleteProduct(@PathParam("id") long productID);
 
-	public List<Campaign> getCampaignsForProduct(long productID);
+	@GET
+	@Path("/{id}/campaigns")
+	public List<Campaign> getCampaignsForProduct(@PathParam("id") long productID);
 
 }
